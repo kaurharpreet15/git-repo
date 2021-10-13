c254121d9f6f23a388085387e4743a0df2a59a31
kaurharpreet15 <68943045+kaurharpreet15@users.noreply.github.com>
2021-10-14 02:04:14 +0530

    added featureblock in module subnet main.tf file

--- terraform/subnet/main.tf
+++ terraform/subnet/main.tf
@@ -1,8 +1,11 @@
+provider "azurerm" {
+    features()
+}
 
-    resource "azurerm_subnet" "subnet"{
-        name = var.subnet_name
-        resource_group_name = var.vnet_rg_name
-        virtual_network_name = var.vnet_name
-        address_prefix = var.address_prefix
+resource "azurerm_subnet" "subnet"{
+    name = var.subnet_name
+    resource_group_name = var.vnet_rg_name
+    virtual_network_name = var.vnet_name
+    address_prefix = var.address_prefix
 
     }
