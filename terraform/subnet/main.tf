1333a2b071c82025d03b80c17b936c218fd6d0ca
kaurharpreet15 <68943045+kaurharpreet15@users.noreply.github.com>
2021-10-14 12:38:13 +0530

    corrections for subnet

--- terraform/subnet/main.tf
+++ terraform/subnet/main.tf
@@ -6,5 +6,5 @@ resource "azurerm_subnet" "subnet"{
     name = var.subnet_name
     resource_group_name = var.vnet_rg_name
     virtual_network_name = var.vnet_name
-    address_prefixes = var.address_prefix
+    address_prefix = var.address_prefix
     }
